620dcfc4c1a0960872a70d3904399f565fe45533
lineconquista <lineconquista2010@hotmail.com>
2022-11-20 01:57:10 -0300

    removendo imfilebrowser

--- examples/earth/model.cpp
+++ examples/earth/model.cpp
@@ -2,7 +2,6 @@
 
 #include <unordered_map>
 
-// Explicit specialization of std::hash for Vertex
 template <> struct std::hash<Vertex> {
   size_t operator()(Vertex const &vertex) const noexcept {
     auto const h1{std::hash<glm::vec3>()(vertex.position)};
@@ -12,30 +11,24 @@ template <> struct std::hash<Vertex> {
 };
 
 void Model::computeNormals() {
-  // Clear previous vertex normals
   for (auto &vertex : m_vertices) {
     vertex.normal = glm::vec3(0.0f);
   }
 
-  // Compute face normals
   for (auto const offset : iter::range(0ULL, m_indices.size() , 3ULL)) {
-    // Get face vertices
     auto &a{m_vertices.at(m_indices.at(offset + 0))};
     auto &b{m_vertices.at(m_indices.at(offset + 1))};
     auto &c{m_vertices.at(m_indices.at(offset + 2))};
 
-    // Compute normal
     auto const edge1{b.position - a.position};
     auto const edge2{c.position - b.position};
     auto const normal{glm::cross(edge1, edge2)};
 
-    // Accumulate on vertices
     a.normal += normal;
     b.normal += normal;
     c.normal += normal;
   }
 
-  // Normalize
   for (auto &vertex : m_vertices) {
     vertex.normal = glm::normalize(vertex.normal);
   }
@@ -44,24 +37,17 @@ void Model::computeNormals() {
 }
 
 void Model::createBuffers() {
-  // Delete previous buffers
   abcg::glDeleteBuffers(1, &m_EBO);
   abcg::glDeleteBuffers(1, &m_VBO);
 
-  // VBO
   abcg::glGenBuffers(1, &m_VBO);
   abcg::glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-  abcg::glBufferData(GL_ARRAY_BUFFER,
-                     sizeof(m_vertices.at(0)) * m_vertices.size(),
-                     m_vertices.data(), GL_STATIC_DRAW);
+  abcg::glBufferData(GL_ARRAY_BUFFER, sizeof(m_vertices.at(0)) * m_vertices.size(), m_vertices.data(), GL_STATIC_DRAW);
   abcg::glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-  // EBO
   abcg::glGenBuffers(1, &m_EBO);
   abcg::glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
-  abcg::glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-                     sizeof(m_indices.at(0)) * m_indices.size(),
-                     m_indices.data(), GL_STATIC_DRAW);
+  abcg::glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(m_indices.at(0)) * m_indices.size(), m_indices.data(), GL_STATIC_DRAW);
   abcg::glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
@@ -88,39 +74,26 @@ void Model::loadObj(std::string_view path, bool standardize) {
 
   m_hasNormals = false;
 
-  // A key:value map with key=Vertex and value=index
   std::unordered_map<Vertex, GLuint> hash{};
 
-  // Loop over shapes
   for (auto const &shape : shapes) {
-    // Loop over indices
     for (auto const offset : iter::range(shape.mesh.indices.size())) {
-      // Access to vertex
       auto const index{shape.mesh.indices.at(offset)};
 
-      // Vertex position
       auto const startIndex{3 * index.vertex_index};
-      glm::vec3 position{attrib.vertices.at(startIndex + 0),
-                         attrib.vertices.at(startIndex + 1),
-                         attrib.vertices.at(startIndex + 2)};
+      glm::vec3 position{attrib.vertices.at(startIndex + 0), attrib.vertices.at(startIndex + 1), attrib.vertices.at(startIndex + 2)};
 
-      // Vertex normal
       glm::vec3 normal{};
       if (index.normal_index >= 0) {
         m_hasNormals = true;
         auto const normalStartIndex{3 * index.normal_index};
-        normal = {attrib.normals.at(normalStartIndex + 0),
-                  attrib.normals.at(normalStartIndex + 1),
-                  attrib.normals.at(normalStartIndex + 2)};
+        normal = {attrib.normals.at(normalStartIndex + 0), attrib.normals.at(normalStartIndex + 1), attrib.normals.at(normalStartIndex + 2)};
       }
 
       Vertex const vertex{.position = position, .normal = normal};
 
-      // If hash doesn't contain this vertex
       if (!hash.contains(vertex)) {
-        // Add this index (size of m_vertices)
         hash[vertex] = m_vertices.size();
-        // Add this vertex
         m_vertices.push_back(vertex);
       }
 
@@ -151,44 +124,32 @@ void Model::render(int numTriangles) const {
 }
 
 void Model::setupVAO(GLuint program) {
-  // Release previous VAO
   abcg::glDeleteVertexArrays(1, &m_VAO);
 
-  // Create VAO
   abcg::glGenVertexArrays(1, &m_VAO);
   abcg::glBindVertexArray(m_VAO);
 
-  // Bind EBO and VBO
   abcg::glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
   abcg::glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
 
-  // Bind vertex attributes
-  auto const positionAttribute{
-      abcg::glGetAttribLocation(program, "inPosition")};
+  auto const positionAttribute{abcg::glGetAttribLocation(program, "inPosition")};
   if (positionAttribute >= 0) {
     abcg::glEnableVertexAttribArray(positionAttribute);
-    abcg::glVertexAttribPointer(positionAttribute, 3, GL_FLOAT, GL_FALSE,
-                                sizeof(Vertex), nullptr);
+    abcg::glVertexAttribPointer(positionAttribute, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), nullptr);
   }
 
   auto const normalAttribute{abcg::glGetAttribLocation(program, "inNormal")};
   if (normalAttribute >= 0) {
     abcg::glEnableVertexAttribArray(normalAttribute);
     auto const offset{offsetof(Vertex, normal)};
-    abcg::glVertexAttribPointer(normalAttribute, 3, GL_FLOAT, GL_FALSE,
-                                sizeof(Vertex),
-                                reinterpret_cast<void *>(offset));
+    abcg::glVertexAttribPointer(normalAttribute, 3, GL_FLOAT, GL_FALSE,  sizeof(Vertex), reinterpret_cast<void *>(offset));
   }
 
-  // End of binding
   abcg::glBindBuffer(GL_ARRAY_BUFFER, 0);
   abcg::glBindVertexArray(0);
 }
 
 void Model::standardize() {
-  // Center to origin and normalize largest bound to [-1, 1]
-
-  // Get bounds
   glm::vec3 max(std::numeric_limits<float>::lowest());
   glm::vec3 min(std::numeric_limits<float>::max());
   for (auto const &vertex : m_vertices) {
@@ -196,7 +157,6 @@ void Model::standardize() {
     min = glm::min(min, vertex.position);
   }
 
-  // Center and scale
   auto const center{(min + max) / 2.0f};
   auto const scaling{2.0f / glm::length(max - min)};
   for (auto &vertex : m_vertices) {
